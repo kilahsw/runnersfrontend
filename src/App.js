66f704da8acbd4dbf8b68338f7ced6cb652b064e
Kilah Willingham <kilahmay@kilahs-air.lan>
2020-10-20 22:13:41 -0700

    app.js update

--- src/App.js
+++ src/App.js
@@ -7,26 +7,106 @@ import Form from "./Form";
 function App() {
   //url variable
   const url = "https://runnersbackend.herokuapp.com"
-  
+
   //state to hold shoes
   const [shoes, setShoes] = React.useState([])
 
+  //empty shoe form
+  const emptyShoes = {
+    name: "",
+    runType: "",
+    img: ""
+  }
+
+  //select shoe
+
+  const [selectedShoes, setSelectedShoes] = React.useState(emptyShoes)
+
+  //fetch shoes
+  const getShoes = () => {
+    fetch(url + "/shoes/")
+      .then(response => response.json())
+      .then(data => {
+        setShoes(data)
+      })
+  }
+
+  //get shoes on page
+  React.useEffect(() => {
+    getShoes()
+  }, [])
+
+  //handle create to creat a shoe on submit
+  const handleCreate = (newShoe) => {
+    fetch(url + "/shoes/", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newShoe)
+    })
+      .then(respone => getShoes())
+  }
+
+  //update shoe when form is clicked
+  const handleUpdate = (shoes) => {
+    fetch(url + "/shoes/" + shoes._id, {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(shoes)
+    })
+      .then(response => getShoes())
+  }
+
+  //select a shoe
+
+  const selectShoes = (shoes) => {
+    setSelectedShoes(shoes)
+  }
+
+  //delete a shoe
+  const deleteShoes = (shoes) => {
+    fetch(url + "/shoes" + shoes._id, {
+      method: "delete"
+    })
+      .then(response => getShoes())
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Runners and Shoes</h1>
+      <hr />
+      <Link to="/create">
+        <button>Add Shoes</button>
+      </Link>
+      <main>
+        <Switch>
+          <Route exact path="/"
+            render={(rp) => (
+              <Display
+                {...rp}
+                shoes={shoes}
+                selectShoes={selectShoes}
+                deleteShoes={deleteShoes} />
+            )}
+          />
+          <Route exact path="/create"
+            render={(rp) => (
+              <Form {...rp} label="create" shoes={emptyShoes}
+                handleSubmit={handleCreate} />
+            )}
+          />
+          <Route exact path="/edit"
+            render={(rp) => (
+              <Form {...rp} label="update" shoes={selectedShoes}
+                handleSubmit={handleUpdate} />
+            )}
+          />
+        </Switch>
+      </main>
     </div>
   );
 }
